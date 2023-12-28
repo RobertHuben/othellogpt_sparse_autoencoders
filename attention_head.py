several improvements including second evaluation metric (kl divergence from correct distribution)

attention_head.py
@@ -7,22 +7,22 @@ class MyAttentionHead(torch.nn.Module):
         super().__init__()
         self.d_model=d_model
         self.d_head=d_head
-        scaling_factor=1/math.sqrt(d_model)
-        self.Q = torch.nn.Parameter(scaling_factor*torch.randn((d_model, d_head)))
-        self.K = torch.nn.Parameter(scaling_factor*torch.randn((d_model, d_head)))
-        self.O = torch.nn.Parameter(scaling_factor*torch.randn((d_model, d_head)))
-        self.V = torch.nn.Parameter(scaling_factor*torch.randn((d_model, d_head)))
+        self.scaling_factor=1/math.sqrt(d_model)
+        self.Q = torch.nn.Linear(d_model, d_head, bias=False) 
+        self.K = torch.nn.Linear(d_model, d_head, bias=False)
+        self.V = torch.nn.Linear(d_model, d_head, bias=False)
+        self.O = torch.nn.Linear(d_head, d_model, bias=False)
         self.use_mask=use_mask
 
     def forward(self, residual_stream):
         attention=self.attention_pattern(residual_stream)
-        x=(residual_stream@self.V)@torch.transpose(self.O, dim0=0, dim1=1)
+        x=self.O(self.V(residual_stream))
         return attention@x
 
     def attention_pattern(self, residual_stream):
-        keys=residual_stream@self.K
-        queries=residual_stream@self.Q
-        pre_attention=queries@torch.transpose(keys, dim0=1, dim1=2)
+        keys=self.K(residual_stream)
+        queries=self.Q(residual_stream)
+        pre_attention=queries@torch.transpose(keys, dim0=1, dim1=2)*self.scaling_factor
         if self.use_mask:
             upper_triangular=torch.tril(torch.ones(pre_attention.shape))
             pre_attention=pre_attention.masked_fill(upper_triangular==0, float('-inf'))

othello_gpt.py
@@ -18,9 +18,9 @@ class OthelloGPT(nn.Module):
         self.position_embed_table=nn.Embedding(window_length, d_model)
         self.unembed=nn.Linear(d_model, vocab_size)
 
-        self.linear_encoders=[nn.Linear(in_features=d_model, out_features=4*d_model, bias=True)]
+        self.linear_encoders=[nn.Linear(in_features=d_model, out_features=4*d_model, bias=True) for _ in range(num_layers)]
         self.linear_activation=nn.GELU()
-        self.linear_decoders=[nn.Linear(in_features=4*d_model, out_features=d_model, bias=True)]
+        self.linear_decoders=[nn.Linear(in_features=4*d_model, out_features=d_model, bias=True) for _ in range(num_layers)]
         self.attention_sublayers=[MyMultiHeadAttention(d_model=self.d_model, n_heads=self.n_heads, use_mask=True) for _ in range(num_layers)]
 
 
@@ -29,7 +29,7 @@ class OthelloGPT(nn.Module):
         '''
         if input.shape[1]>self.window_length:
             input=input[:,:self.window_length]
-        if targets.shape[1]>self.window_length:
+        if targets != None and targets.shape[1]>self.window_length:
             targets=targets[:,:self.window_length]
         positions=torch.arange(self.window_length)
         logits=self.token_embed_table(input)+self.position_embed_table(positions)

train.py
@@ -1,5 +1,8 @@
 import torch
 from utils.tokenizer import load_data
+from utils.game_engine import history_to_legal_moves
+from functools import cache
+
 
 def tokens_list():
     tokens=[f"{letter}{number}" for letter in "ABCDEFGH" for number in range(1,9)]
@@ -9,14 +12,15 @@ def tokens_list():
 
 vocab=tokens_list()
 vocab_size=len(vocab)
-block_size=64
+# block_size=64
 batch_size=8
+max_iter=10000
 train_data, val_data=load_data()
 split_token_index=vocab.index("XX")
 split_points_train=torch.tensor([position for position, token in enumerate(train_data) if token==split_token_index])
 split_points_test=torch.tensor([position for position, token in enumerate(val_data) if token==split_token_index])
 
-def get_batch(split):
+def get_batch(split, block_size, batch_size=batch_size):
     # generate a small batch of data of inputs x and targets y
     data = train_data if split == 'train' else val_data
     split_points = split_points_train if split == 'train' else split_points_test
@@ -31,14 +35,29 @@ def train_model(model):
     torch.manual_seed(1337)
 
     optimizer=torch.optim.AdamW(model.parameters(), lr=1e-3)
-    steps_to_print=1
-    for step in range(10000):
-        xb,yb=get_batch("train")
+    steps_to_print_on=[500*x for x in range(1, max_iter//500)]
+    for step in range(max_iter):
+        xb,yb=get_batch("train", block_size=model.window_length)
         logits, loss=model(xb, yb)
         optimizer.zero_grad(set_to_none=True)
         loss.backward()
         optimizer.step()
-        if step==steps_to_print:
-            print(f"Loss after {step} steps: {loss.item()}")
-            steps_to_print*=2
+        if step in steps_to_print_on:
+            divergence=evaluate_kl_divergence(model, num_samples=100)
+            print(f"Loss and divergence after {step} steps: {loss.item():.4f}, {divergence:.4f}")
+
+@cache
+def get_data_and_legal_move_distribution(window_length, num_samples):
+    xb,yb=get_batch("test", window_length, num_samples)
+    legal_moves=history_to_legal_moves(xb)
+    legal_move_distribution=legal_moves/legal_moves.sum(dim=-1, keepdim=True)
+    return xb, legal_move_distribution
+
+def evaluate_kl_divergence(model, num_samples=10):
+    xb, legal_move_distribution=get_data_and_legal_move_distribution(window_length=model.window_length, num_samples=num_samples)
+    logits, loss=model(xb, None)
+    kl_loss=torch.nn.KLDivLoss(reduction='batchmean')
+    log_softmax=torch.nn.LogSoftmax(dim=-1)
+    divergence=kl_loss(log_softmax(logits), legal_move_distribution)
+    return float(divergence)
 