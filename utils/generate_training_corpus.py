ed1c39e319b4dfc3c8e7eced8db8d5f9ea5f0e76
Robert <rvhuben@gmail.com>
2023-12-30 07:40:31 -0500

    allowed for more generation

--- utils/generate_training_corpus.py
+++ utils/generate_training_corpus.py
@@ -1,18 +1,22 @@
 from random import seed
-from utils.game_engine import generate_random_game, move_log_to_string
+from game_engine import generate_random_game, move_log_to_string
 import time
 
 
-def generate_training_corpus(num_games=100, random_seed=42, to_write_location="training_corpus.txt"):
+def generate_training_corpus(num_games=100, random_seed=42, to_write_location="training_corpus.txt", write_mode='w'):
     seed(random_seed)
     games_as_list=[move_log_to_string(generate_random_game(), insert_terminal_XX=True) for _ in range(num_games)]
     games_as_string="\n".join(games_as_list)
-    with open(to_write_location, 'w') as f:
+    with open(to_write_location, write_mode) as f:
+        if write_mode=="a":
+            f.write("\n")
         f.write(games_as_string)
     return
 
 if __name__=="__main__":
-    start=time.time()
-    generate_training_corpus(10000)
-    end=time.time()
-    print(f"Time for 10000: {end-start} seconds")
+    for n in range(80):
+        start=time.time()
+        k=1000
+        generate_training_corpus(k, write_mode='a')
+        end=time.time()
+        print(f"Time for {k}: {end-start} seconds")
