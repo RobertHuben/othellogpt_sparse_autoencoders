2ec86c09468726c52054698453e2be859ec885e0
Robert <rvhuben@gmail.com>
2024-01-01 20:28:39 -0500

    fixed tied embeddings

--- autoencoder.py
+++ autoencoder.py
@@ -5,15 +5,18 @@ from torch.nn import functional as F
 
 class SparseAutoencoder(nn.Module):
 
-    def __init__(self, input_size, feature_ratio, sparsity_coeff=.1):
+    def __init__(self, input_size, feature_ratio, sparsity_coeff=.1, initialization_weights=None):
         super().__init__()
         self.input_size=input_size
         self.sparsity_coeff=sparsity_coeff
         self.hidden_layer_size=int(input_size*feature_ratio)
+        if initialization_weights==None:
+            initialization_weights=torch.normal(0, 1, (self.input_size, self.hidden_layer_size))
         self.encoder=nn.Linear(self.input_size, self.hidden_layer_size)
+        self.encoder.weight=nn.Parameter(initialization_weights.transpose(0,1))
         self.decoder=nn.Linear(self.hidden_layer_size, self.input_size, bias=False)
-        self.decoder.weight[:] = self.encoder.weight.T[:] #tied weights
-        self.activation=F.relu()
+        self.decoder.weight=nn.Parameter(initialization_weights)
+        self.activation=torch.nn.ReLU()
 
 
     def forward(self, input):
