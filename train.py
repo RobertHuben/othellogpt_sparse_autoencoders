8a09f2dae3c9ebb32d6fdf739ee0610ebf808993
Robert <rvhuben@gmail.com>
2024-01-09 11:52:28 -0500

    made train use tqdm

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import torch
 from utils.dataloaders import get_dataloader
+from tqdm import tqdm
 
 device='cuda' if torch.cuda.is_available() else 'cpu'
 
@@ -18,7 +19,7 @@ def train_model(model, train_dataset_type, eval_dataset_type, batch_size=64, num
     for epoch in range(num_epochs):
         train_dataloader=iter(get_dataloader(mode=train_dataset_type, window_length=model.window_length, batch_size=batch_size))
         print(f"Beginning epoch {epoch+1}/{num_epochs}. Epoch duration is {len(train_dataloader)} steps.")
-        for input_batch,label_batch in train_dataloader:
+        for input_batch,label_batch in tqdm(train_dataloader):
             step+=1
             optimizer.zero_grad(set_to_none=True)
             logits, loss=model(input_batch, label_batch)
