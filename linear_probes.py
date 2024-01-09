slightly corrected accuracy calculation

linear_probes.py
@@ -33,19 +33,20 @@ class LinearProbe(torch.nn.Module):
     def print_evaluation(self, train_loss, eval_dataset_type, step_number="N/A", details=False):
         del details
         accuracy=self.evaluate_top_one_board_state_accuracy(eval_dataset_type)
-        print(f"Train loss and test accuracy after {step_number} steps: {train_loss.item():.4f}, {accuracy:.4f}")
+        print(f"Train loss and test accuracy after {step_number} steps: {train_loss.item():.4f}, {accuracy:.4%}")
 
-    def evaluate_top_one_board_state_accuracy(self, eval_dataset_type="probe_test", num_samples=80):
-        batch_size=1
-        accuracies=[]
+    def evaluate_top_one_board_state_accuracy(self, eval_dataset_type="probe_test"):
+        batch_size=8
+
+        total_predictions=0
+        total_correct_predictions=0
         window_length=self.window_length
         test_probe_dataloader=iter(get_dataloader(mode=eval_dataset_type, window_length=window_length, batch_size=batch_size))
 
         for input_batch,label_batch in test_probe_dataloader:
             predictions, loss=self(input_batch)
             one_hot_predictions=predictions.argmax(dim=3)
-            correct_predictions=(label_batch==one_hot_predictions).sum()
-            total_predictions=(label_batch>=0).sum()
-            accuracies.append(correct_predictions/total_predictions)
+            total_correct_predictions+=(label_batch==one_hot_predictions).sum()
+            total_predictions+=(label_batch>=0).sum()
 
-        return float(torch.tensor(accuracies).mean())
+        return float(total_correct_predictions/total_predictions)